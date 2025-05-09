8c1cdb5310c4aa2c9f55ec3403c02bc5de4170d5
ravilella1234 <ravilella.1981@gmail.com>
2025-05-09 08:58:20 +0530

    WebDriver Methods

--- May2025_7_30AM_SeleniumBatch/src/test/java/com/project/TC_001.java
+++ May2025_7_30AM_SeleniumBatch/src/test/java/com/project/TC_001.java
@@ -1,5 +1,12 @@
 package com.project;
 
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
+
 public class TC_001 extends BaseClass
 {
 	
@@ -7,8 +14,57 @@ public class TC_001 extends BaseClass
 	{		
 		init();
 		
-		browserLaunch("firefoxbrowser");
+		browserLaunch("chromebrowser");
 		
 		navigateUrl("amazonurl");
+		
+		driver.manage().window().maximize();
+		
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+		
+		String page = driver.getPageSource();
+		System.out.println(page);
+		
+		String hid1 = driver.getWindowHandle();
+		System.out.println(hid1);
+		
+		driver.switchTo().newWindow(WindowType.TAB);
+		driver.get("https://www.flipkart.com/");
+		Set<String> hid2 = driver.getWindowHandles();
+		for(String h:hid2)
+		{
+			System.out.println(h);
+		}
+		
+		System.out.println(driver.getTitle());
+		
+		
+		//driver.navigate().back();
+		
+		//Thread.sleep(4000);
+		
+		//driver.navigate().forward();
+		
+		//Thread.sleep(4000);
+		
+		//driver.navigate().refresh();
+		
+		//driver.close();
+		
+		//driver.quit();
+		
+		
+		String text = driver.findElement(By.linkText("Cart")).getText();
+		System.out.println(text);
+		
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		for(int i=0;i<links.size();i++)
+		{
+			System.out.println(links.get(i).getText());
+		}
 	}
 }
