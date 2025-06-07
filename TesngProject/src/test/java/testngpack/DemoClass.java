291fd896f23ad18b9ad71b9bceafdbb559a166d4
ravilella1234 <ravilella.1981@gmail.com>
2025-06-07 09:02:50 +0530

    TestNG Day-2

---
+++ TesngProject/src/test/java/testngpack/DemoClass.java
@@ -0,0 +1,14 @@
+package testngpack;
+
+import org.testng.annotations.Test;
+
+public class DemoClass 
+{
+	
+	@Test
+	public void m1()
+	{
+		System.out.println("m1");
+	}
+
+}
---
+++ TesngProject/src/test/java/testngpack/NewTest.java
@@ -0,0 +1,14 @@
+package testngpack;
+
+import org.testng.annotations.Test;
+
+public class NewTest 
+{
+	
+  @Test
+  public void f1() 
+  {
+	  System.out.println("iam F test");
+  }
+  
+}
